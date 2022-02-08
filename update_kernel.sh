rsalvaterra/maintainer-tools

commit cbb9d087d6b13bd066a1d2e19ba25f87b06f5200

update_kernel.sh: update it to new kernel hash/version file way
OpenWrt changed how the hash/version of the various kernels are declared and
saved.
They are now placed in dedicated files. Fix the update_kernel.sh script to
update the kernel version/hash in the correct file.

Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>
[fix typos/wrapping]
Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

update_kernel.sh
@@ -155,7 +155,7 @@ if [ "$UPDATE" -eq 1 ]; then
 		CHECKSUM=$(./staging_dir/host/bin/mkhash sha256 dl/linux-$PATCHVER.tar.xz)
 	fi
 
-	$CMD ./staging_dir/host/bin/sed -i include/kernel-version.mk \
+	$CMD ./staging_dir/host/bin/sed -i include/kernel-${KERNEL} \
 		-e "s|LINUX_VERSION-${KERNEL} =.*|LINUX_VERSION-${KERNEL} = ${NEWVER}|" \
 		-e "s|LINUX_KERNEL_HASH-${KERNEL}.*|LINUX_KERNEL_HASH-${PATCHVER} = ${CHECKSUM}|"
 fi

commit 8b1e7657eb019546622a9901b0c8adc3a2053220

update_kernel.sh: fix unified version file updates
The previous commit broke the kernel-version.mk automated update, since the
kernel version files are now split. However, older branches still use the
unified file, so compatibility must be kept.

Check for the presence of the kernel version-specific file. If it doesn't exist,
assume we're using the unified file and do the version update accordingly.

Fixes: cbb9d08 ("update_kernel.sh: update it to new kernel hash/version file way")

Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

update_kernel.sh
@@ -155,7 +155,15 @@ if [ "$UPDATE" -eq 1 ]; then
 		CHECKSUM=$(./staging_dir/host/bin/mkhash sha256 dl/linux-$PATCHVER.tar.xz)
 	fi
 
-	$CMD ./staging_dir/host/bin/sed -i include/kernel-${KERNEL} \
+	if [ -f include/kernel-${KERNEL} ]; then
+		# split version files
+		KERNEL_VERSION_FILE=include/kernel-${KERNEL}
+	else
+		# unified version file
+		KERNEL_VERSION_FILE=include/kernel-version.mk
+	fi
+
+	$CMD ./staging_dir/host/bin/sed -i ${KERNEL_VERSION_FILE} \
 		-e "s|LINUX_VERSION-${KERNEL} =.*|LINUX_VERSION-${KERNEL} = ${NEWVER}|" \
 		-e "s|LINUX_KERNEL_HASH-${KERNEL}.*|LINUX_KERNEL_HASH-${PATCHVER} = ${CHECKSUM}|"
 fi